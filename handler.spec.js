3692ecdc87c9db9c797c8c681756e86fdac514ca
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-13 12:44:13 +0530

    Added

--- handler.spec.js
+++ handler.spec.js
@@ -9,7 +9,7 @@ describe("The handler function", () => {
     it("returns a message", () => {
         handler.hello(undefined, undefined, function(error, response){
             let body = JSON.parse(response.body);
-            /body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully! lo');
+            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully! lo');
             //expect(body.message).to.equal('Go Serverless v1.0! Your function executed successfully!');
             //done();
         });
