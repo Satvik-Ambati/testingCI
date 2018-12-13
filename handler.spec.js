a1d478a36d7fecd212d3ff73b4e59c5066ff9fa0
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-13 11:20:44 +0530

    test should fail

--- handler.spec.js
+++ handler.spec.js
@@ -8,7 +8,7 @@ describe("The handler function", () => {
     it("returns a message", () => {
         handler.hello(undefined, undefined, function(error, response){
             let body = JSON.parse(response.body);
-            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!');
+            body.message.should.be.equal('Go Serverless v1.0! Your function executed successfully!  ');
         });
     });
 });
